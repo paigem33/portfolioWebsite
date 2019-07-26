1e4fd6357bd4674ee3ea38b0f0af15e3d6384789
paigem33 <paigemacgregor33@gmail.com>
2019-07-25 22:27:21 -0700

    worked on changes

--- portfolioTakeTwo/script.js
+++ portfolioTakeTwo/script.js
@@ -15,42 +15,42 @@ menuFunc = () => {
 }
 
 
-var html, body, scrollToTopButton;
-window.onload = function() {
-  html = document.documentElement;
-  body = document.body;
-  scrollToTopButton = document.getElementById("scrollToTopButton");
-};
-
-function scrollToTop(totalTime, easingPower) {
-  //console.log("here");
-  var timeInterval = 1; //in ms
-  var scrollTop = Math.round(body.scrollTop || html.scrollTop);
-  //var by=- scrollTop;
-  var timeLeft = totalTime;
-  var scrollByPixel = setInterval(function() {
-    var percentSpent = (totalTime - timeLeft) / totalTime;
-    if (timeLeft >= 0) {
-      var newScrollTop = scrollTop * (1 - easeInOut(percentSpent, easingPower));
-      body.scrollTop = newScrollTop;
-      html.scrollTop = newScrollTop;
-      //console.log(easeInOut(percentSpent,easingPower));
-      timeLeft--;
-    } else {
-      clearInterval(scrollByPixel);
-      //Add hash to the url after scrolling
-      //window.location.hash = hash;
-    }
-  }, timeInterval);
-}
-
-function easeInOut(t, power) {
-  if (t < 0.5) {
-    return 0.5 * Math.pow(2 * t, power);
-  } else {
-    return 0.5 * (2 - Math.pow(2 * (1 - t), power));
-  }
-}
+//var html, body, scrollToTopButton;
+//window.onload = function() {
+//  html = document.documentElement;
+//  body = document.body;
+//  scrollToTopButton = document.getElementById("scrollToTopButton");
+//};
+//
+//function scrollToTop(totalTime, easingPower) {
+//  //console.log("here");
+//  var timeInterval = 1; //in ms
+//  var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+//  //var by=- scrollTop;
+//  var timeLeft = totalTime;
+//  var scrollByPixel = setInterval(function() {
+//    var percentSpent = (totalTime - timeLeft) / totalTime;
+//    if (timeLeft >= 0) {
+//      var newScrollTop = scrollTop * (1 - easeInOut(percentSpent, easingPower));
+//      body.scrollTop = newScrollTop;
+//      html.scrollTop = newScrollTop;
+//      //console.log(easeInOut(percentSpent,easingPower));
+//      timeLeft--;
+//    } else {
+//      clearInterval(scrollByPixel);
+//      //Add hash to the url after scrolling
+//      //window.location.hash = hash;
+//    }
+//  }, timeInterval);
+//}
+//
+//function easeInOut(t, power) {
+//  if (t < 0.5) {
+//    return 0.5 * Math.pow(2 * t, power);
+//  } else {
+//    return 0.5 * (2 - Math.pow(2 * (1 - t), power));
+//  }
+//}
 
 //function debounce(func, wait = 20, immediate = true) {
 //      var timeout;
